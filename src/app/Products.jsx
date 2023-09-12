3624493b3d8bd74e4c5c8d36026f81f89be4cc60
narinda.hcs <narinda.hcs@gmail.com>
2023-09-12 11:51:17 +0800

    Product section

--- src/app/Products.jsx
+++ src/app/Products.jsx
@@ -1,7 +1,41 @@
+import Image from "next/image";
+
 export default function Products() {
+  const products = ["Product 1", "Product 2", "Product 3", "Product 4"];
   return (
-    <section>
-      
+    <section className="h-screen w-screen flex flex-col items-center justify-center p-24">
+      <h2 className="text-5xl font-extrabold">Product dari halaman partial</h2>
+      <div className="bg-blue-500 h-full w-full mt-10 carousel carousel-center rounded-box">
+       
+        {products.map((product, index) => (
+          <div className="carousel-item relative w-full flex flex-col justify-center items-center bg-orange-300" id={`CarosProduct${index}`}> 
+            <Image
+              src="/next.svg"
+              width={400}
+              height={400}
+              alt="Picture of the author"
+              className="bg-blue-200"
+            />
+            <div>
+              <div className="bg-slate-400 w-full flex flex-col justify-center">
+                <h3 className="text-center">{product}</h3>
+                <h4 className="text-center"> deskripsi {product} {products.length}</h4>
+                <p>{`CarosProduct${index === 0? products.length: index-1}`}</p>
+                <p>{`CarosProduct${index}`}</p>
+              </div>
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          
+                <a href={`#CarosProduct${index === 0? products.length-1: index-1}`} className="btn btn-circle">
+                  ❮
+                </a>
+                <a href={`#CarosProduct${index === products.length-1? 0: index+1}`} className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,111 @@
 import Image from "next/image";
 import Welcome from "./welcome";
+import Products from "./Products";
 
 export default function Home() {
+  const products  = ['Product 1', 'Product 2', 'Product 3', 'Product 4']
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen max-w-full flex-col items-center justify-between">
       <Welcome />
-      <section className="min-h-screen flex flex-col items-center justify-center px-24">
-        
+      <Products />
+      <section className="h-screen w-screen flex flex-col items-center justify-center p-24">
+        <h2 className="text-5xl font-extrabold">Product</h2>
+        <div className="bg-blue-500 h-full w-full mt-10 carousel carousel-center rounded-box">
+          
+          <div
+            className="carousel-item relative w-full flex flex-col justify-center items-center bg-orange-300"
+            id="CarProduct1"
+          >
+            <Image
+              src="/next.svg"
+              width={400}
+              height={400}
+              alt="Picture of the author"
+              className="bg-blue-200"
+            />
+            <div>
+              <div className="bg-slate-400 w-full flex flex-col justify-center">
+                <h3 className="text-center">nama produk 1</h3>
+                <h4 className="text-center"> deskripsi produk 1
+                  {products.map((product) => (
+                    <p>{product}</p>
+                  ))}
+                </h4>
+              </div>
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#CarProduct4" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#CarProduct2" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="carousel-item relative w-full" id="CarProduct2">
+            <Image
+              src="/next.svg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+            <div>
+              <h3 className="text-center">nama produk 1</h3>
+              <h4 className="text-center"> deskripsi produk 1</h4>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#CarProduct1" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#CarProduct3" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+          </div>
+
+          <div className="carousel-item relative w-full" id="CarProduct3">
+            <Image
+              src="/next.svg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+            <div>
+              <h3>nama produk 1</h3>
+              <h4> deskripsi produk 1</h4>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#CarProduct2" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#CarProduct4" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+          </div>
+
+          <div className="carousel-item relative w-full" id="CarProduct4">
+            <Image
+              src="/next.svg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+            <div>
+              <h3>nama produk 1</h3>
+              <h4> deskripsi produk 1</h4>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#CarProduct3" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#CarProduct1" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+          </div>
+        </div>
       </section>
     </main>
   );
